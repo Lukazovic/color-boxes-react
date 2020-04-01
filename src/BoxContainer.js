a46bec207753d1a9bffbc447d771c69b4ba97212
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-04-01 19:21:08 -0300

    Update

--- src/BoxContainer.js
+++ src/BoxContainer.js
@@ -5,10 +5,11 @@ import './BoxContainer.css'
 
 class BoxContainer extends Component{
   static defaultProps = {
-    numBoxes: 18
+    numBoxes: 18,
+    allColors: ['purple', 'magenta', 'violet', 'pink']
   }
   render(){
-    const boxes = Array.from({length: this.props.numBoxes}).map(() => <Box />)
+    const boxes = Array.from({length: this.props.numBoxes}).map(() => <Box colors={this.props.allColors}/>)
     return (
       <div className='BoxContainer'>{boxes}</div>
     )
